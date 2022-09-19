328016d5432dabc608b113f9c538d358fa8522d4
Pablo <pablogonzalvezdelpino99@gmail.com>
2022-09-19 20:32:10 +0200

    Init Proyect

---
+++ MyFirstApp_Pablo/MyFirstApp_Pablo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp_Pablo
+//
+//  Created by Alumnos on 19/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    let variable = "joseluis"
+}
+
