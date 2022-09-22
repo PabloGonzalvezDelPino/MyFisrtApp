fe9ee96067f8e9b838b3daa3e8887815e36b8188
Pablo Gonzalvez <pablogonzalvezdelpino99@gmail.com>
2022-09-22 20:49:10 +0200

    Bucles

--- MyFirstApp_Pablo/MyFirstApp_Pablo/ViewController.swift
+++ MyFirstApp_Pablo/MyFirstApp_Pablo/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("con FOR")
+        forSentence()
+        print("con WHILE")
+        whileSentence()
+        print("con REPEAT")
+        repeatSentence()
     }
+    func forSentence(){
+        for i in 1...5{
+            print(i)
+        }
+    }
+    func whileSentence(){
+        var i = 1
+        while i<6{
+            print(i)
+            i += 1
+        }
+        
+    }
+    func repeatSentence(){
+        var i = 1
+        repeat{
+            print(i)
+            i+=1
+        }while i < 6
+        
+    }
+    
+    func recibeNumbers(){
+        let num = 6
+        switch num{
+        case 0...4:
+            print("suspenso")
+        case 5...6:
+            print("aprobado")
+        case 7...8:
+            print("notable")
+        case 9...10:
+            print("sobresaliente")
+        default:
+            print("Te fumaste un siberiano")
+        }
+        
+    }
+    
 
-    let variable = "jaimeEnrique"
+    
+    
+    
 }
 
